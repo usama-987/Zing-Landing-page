707439f5c5a49771bbde1c03be92a6772407f284
usama-987 <usamas406@gmail.com>
2026-04-24 23:07:56 +0500

    Push all fikes

--- src/features/components/header/Navbar.jsx
+++ src/features/components/header/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Logo from "..//..//..//assets/logo.png";
+import Logo from "../../../assets/logo.png";
 import arrow from "../../../assets/SVG.png";
 
 const defaultNavLinks = [
